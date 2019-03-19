79b63fd2c7781307107dede08f38d90596ff7c0c
Administrator <1757286697@qq.com>
2019-03-20 00:21:29 +0800

    环境没有提交,想来是没有问题的

---
+++ booktest/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from .models import *
+
+
+# 来定义模型在Admin界面的显示方式
+class BookInfoAdmin(admin.ModelAdmin):
+    # list_display：显示字段，可以点击列头进行排序
+    # list_display = ['id', 'btitle', 'bpub_date']
+    list_display = [ 'btitle', 'bpub_date']
+
+    # list_filter：过滤字段，过滤框会出现在右侧
+    list_filter = ['btitle']
+
+    # search_fields：搜索字段，搜索框会出现在上侧
+    # search_fields = ['btitle']
+    # list_per_page：分页，分页框会出现在下侧
+    list_per_page = 10
+    # 添加、修改页属性
+    # fields：属性的先后顺序
+    # fields = ['bpub_date', 'btitle']
+    # fieldsets：属性分组
+    fieldsets = [
+        ('basic', {'fields': ['btitle']}),
+        ('more', {'fields': ['bpub_date']}),
+    ]
+
+
+# Register your models here.
+
+# admin.site.register()
+admin.site.register(HeroInfo)
+admin.site.register(BookInfo, BookInfoAdmin)
+# admin.site.register(BookInfo)
---
+++ booktest/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+# Create your models here.
+# 根据定义,生出特殊语句并创建表
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=22)
+    bpub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.btitle#.encode('utf-8')
+
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=11)
+    hgender = models.BooleanField()
+    hcontent = models.CharField(max_length=1000)
+    # 引用外键，即BookInfo对象
+    hbook = models.ForeignKey('BookInfo', on_delete=models.CASCADE, )
+
+    def __str__(self):
+        return self.btitle#.encode('utf-8')
+    # 即在外键值的后面加上
+    # on_delete = models.CASCADE
+    # 原因：
+    #  https://www.cnblogs.com/phyger/p/8035253.html
+    # 在django2.0 后，定义外键和一对一关系的时候需要加on_delete选项，此参数为了避免两个表里的数据不一致问题，不然会报错：
+    #
+
