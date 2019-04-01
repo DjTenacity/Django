9e4d96fede38935aba02e64a200b48ea6f5bfe5b
Administrator <1757286697@qq.com>
2019-04-02 00:34:10 +0800

    模型查询2

--- DjangoDemo2/lovedj/views.py
+++ DjangoDemo2/lovedj/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Max, F
+from django.db.models import Max, F, Q
 from django.shortcuts import render
 
 # Create your views here.
@@ -8,11 +8,15 @@ from lovedj.models import BookInfo
 def index(request):
     # list=BookInfo.books1.filter(herinfo__hcontnt__contains="十五");
     # 小于  等于e
-    list=BookInfo.books1.filter(pk__lte=3);
-    Max1=BookInfo.books1.aggregate(Max('bpub_date'));
+    list = BookInfo.books1.filter(pk__lte=3);
+    Max1 = BookInfo.books1.aggregate(Max('bpub_date'))
     # 算法 一个列和另一个列比较
-    gt=BookInfo.books1.filter(bread__gt=F("bcommet"))
+    gt = BookInfo.books1.filter(bread__gt=F("bcommet"))
+    list2 = BookInfo.books1.filter(pk_lt=6, htitle__contains="十五")
+    list3 = BookInfo.books1.filter(pk_lt=6).filter( htitle__contains="十五")
+    # 逻辑 或
+    list3 = BookInfo.books1.filter(Q(pk_lt=6)|Q( htitle__contains="十五"))
 
-    context={'list1':list,
-             'Max1':Max1}
-    return render(request,'lovedj/index.html',context)
+    context = {'list1': list,
+               'Max1': Max1}
+    return render(request, 'lovedj/index.html', context)
