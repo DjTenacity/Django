ae4afa85082b559553f39b42a155b86e1fda866b
gdj <haohaowang@be-service.com>
2019-04-22 18:53:55 +0800

    全文检索 修改

--- DjangoDemo5/booktest/search_indexes.py
+++ DjangoDemo5/booktest/search_indexes.py
@@ -1,13 +1,13 @@
 from haystack import indexes
-from booktest.models import BookInfo
+from booktest.models import HeroInfo
 
 
-class GoodsInfoIndex(indexes.SearchIndex, indexes.Indexable):
+class HeroInfoIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
-        return BookInfo
+        return HeroInfo
 
     # BookInfo.objects.all()
     def index_queryset(self, using=None):
-        return self.get_model().objects.filter(isDelete=False)
+        return self.get_model().objects.all()#.filter(isDelete=False)
